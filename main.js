e2e1b0f23b3b6777eb92916046a0ea7629d5bddf
Pat Brokaw <plbrokaw@gmail.com>
2019-07-21 19:32:36 -0500

    Adding tutorial files

---
+++ main.js
@@ -0,0 +1,56 @@
+//Variable types: String, Numbers, Boolean, null, undefined,
+// const name = 'John';
+// const age = 30;
+// // console.log(typeof name);
+// // console.log(typeof age);
+
+// //Concatenate
+// console.log('My name is ' + name + ' and I am ' + age);
+// //Template String
+// const hello = `My name is ${name} and I am ${age}`;
+// console.log(hello);
+
+////Variable properties & methods
+// const s = 'Hello World!';
+// console.log(s.length);
+// console.log(s.toUpperCase());
+// console.log(s.toLowerCase());
+// console.log(s.substring(0, 5));
+// console.log(s.substring(0, 5).toUpperCase());
+// console.log(s.split(''));
+//const s = 'technology, computers, it, code';
+//console.log(s.split(', '));
+
+// //Arrays - variables that hold multiple values
+// const numbers = new Array(1,2,3,4,5);
+// console.log(numbers);
+// const fruits = ['apples', 'oranges', 'pears'];
+// console.log(fruits);
+// console.log(fruits[1]);
+// fruits[3] = 'peaches';
+// console.log(fruits);
+// console.log(fruits[fruits.length-1]);
+// fruits[fruits.length-1] = 'grapes';
+// console.log(fruits);
+// fruits.push('mangos');
+// console.log(fruits);
+// fruits.unshift('strawberries');
+// console.log(fruits);
+// console.log(Array.isArray(fruits));
+// console.log(fruits.indexOf('oranges'));
+
+//Object literals
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: '1233 Sloan Ave',
+        city: 'Palmyra',
+        state: 'MO',
+        zip: 63461
+    }
+}
+
+console.log(person.address.street);
